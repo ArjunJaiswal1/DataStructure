2df8d85fc72f9233b9678e1743deefd2acea7e1b
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-07 19:27:36 +0530

    [Arjun] Add ; Added Switched

---
+++ DataStructure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Data Structure Programs==========");
+
+            Console.WriteLine("SEARCH OPERATIONS !");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.WriteLine("========================================================================================");
+            Console.ReadLine();
+        }
+    }
+}
